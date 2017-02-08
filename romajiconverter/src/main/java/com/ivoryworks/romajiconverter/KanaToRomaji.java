8753f8f5d2f5c83996ca7312332e484c8f20f8fe
ivoryworks <ivoryworkz@gmail.com>
2017-02-09 08:27:39 +0900

    Add Kana2Romaji ヘボン式 拗音変換

--- romajiconverter/src/main/java/com/ivoryworks/romajiconverter/KanaToRomaji.java
+++ romajiconverter/src/main/java/com/ivoryworks/romajiconverter/KanaToRomaji.java
@@ -11,9 +11,11 @@ public class KanaToRomaji {
     public static final int SYSTEM_NIHON = 2;
 
     Map mChokuMap;
+    Map mYouMap;
 
     public KanaToRomaji() {
         loadChokuOnTable();
+        loadYouOnTable();
     }
 
     public List<String> convert(final String str) {
@@ -52,20 +54,52 @@ public class KanaToRomaji {
             return romaList;    // Terminate
         }
 
-        // Choku-on
-        String fig = String.valueOf(str.charAt(index));
-        if (mChokuMap.containsKey(fig)) {
-            List<String> recRoma = convertHepburnRecursion(str, index + 1);
+        if (isYouOn(str, index)) {
+            // You-on
+            String youOn = str.substring(index, index + 2);
+            List<String> recRoma = convertHepburnRecursion(str, index + 2);
             if (recRoma.size() == 0) {
-                romaList.add((String) mChokuMap.get(fig));
+                romaList.add((String) mYouMap.get(youOn));
             } else {
                 for (String childStr : recRoma) {
-                    romaList.add(mChokuMap.get(fig) + childStr);
+                    romaList.add(mYouMap.get(youOn) + childStr);
+                }
+            }
+        } else {
+            // Choku-on
+            String fig = String.valueOf(str.charAt(index));
+            if (mChokuMap.containsKey(fig)) {
+                List<String> recRoma = convertHepburnRecursion(str, index + 1);
+                if (recRoma.size() == 0) {
+                    romaList.add((String) mChokuMap.get(fig));
+                } else {
+                    for (String childStr : recRoma) {
+                        romaList.add(mChokuMap.get(fig) + childStr);
+                    }
                 }
             }
         }
-
         return romaList;
+    }
+
+    private boolean isYouOn(final String str, int index) {
+        // 拗音は2文字必要
+        if ((str.length() - index) < 2) {
+            return false;
+        }
+        String c = String.valueOf(str.charAt(index));
+        String[] youUpper = {"き", "し", "ち", "に", "ひ", "み", "り", "ぎ", "じ", "ぢ", "び", "ぴ"};
+        for (String yu : youUpper) {
+            if (!yu.equals(c)) {
+                continue;
+            }
+            String c2 = String.valueOf(str.charAt(index + 1));
+            // You-on lower
+            if (c2.equals("ゃ") || c2.equals("ゅ") || c2.equals("ょ")) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void loadChokuOnTable() {
@@ -89,6 +123,22 @@ public class KanaToRomaji {
         mChokuMap = new HashMap<>();
         for (int i = 0; i < chokuKana.length; i++) {
             mChokuMap.put(chokuKana[i], chokuRoma[i]);
+        }
+    }
+
+    private void loadYouOnTable() {
+        // 拗音 
+        String[] youKana = {"きゃ", "きゅ", "きょ", "しゃ", "しゅ", "しょ", "ちゃ", "ちゅ", "ちょ",
+                            "にゃ", "にゅ", "にょ", "ひゃ", "ひゅ", "ひょ", "みゃ", "みゅ", "みょ",
+                            "りゃ", "りゅ", "りょ", "ぎゃ", "ぎゅ", "ぎょ", "じゃ", "じゅ", "じょ",
+                            "ぢゃ", "ぢゅ", "ぢょ", "びゃ", "びゅ", "びょ", "ぴゃ", "ぴゅ", "ぴょ"};
+        String[] youRoma = {"kya", "kyu", "kyo", "sha", "shu", "sho", "cha", "chu", "cho",
+                            "nya", "nyu", "nyo", "hya", "hyu", "hyo", "mya", "myu", "myo",
+                            "rya", "ryu", "ryo", "gya", "gyu", "gyo", "ja", "ju", "jo",
+                            "ja", "ju", "jo", "bya", "byu", "byo", "pya", "pyu", "pyo"};
+        mYouMap = new HashMap<>();
+        for (int i = 0; i < youKana.length; i++) {
+            mYouMap.put(youKana[i], youRoma[i]);
         }
     }
 }
--- romajiconverter/src/test/java/com/ivoryworks/romajiconverter/KanaToRomajiHepburnTest.java
+++ romajiconverter/src/test/java/com/ivoryworks/romajiconverter/KanaToRomajiHepburnTest.java
@@ -9,6 +9,34 @@ import static junit.framework.Assert.assertTrue;
 
 public class KanaToRomajiHepburnTest {
     /**
+     * かなtoローマ字 ヘボン式 拗音変換
+     * @throws Exception
+     */
+    @Test
+    public void converterKana2RomajiHepburnYouOn() throws Exception {
+        List<String> result;
+        KanaToRomaji k2r = new KanaToRomaji();
+
+        // sha,shu,sho
+        result = k2r.convert("しゃしゅしょ", KanaToRomaji.SYSTEM_HEPBURN);
+        assertTrue(result.size() == 1);
+        assertEquals(result.get(0), "shashusho");
+
+        // cha,chu,cho
+        result = k2r.convert("ちゃちゅちょ", KanaToRomaji.SYSTEM_HEPBURN);
+        assertTrue(result.size() == 1);
+        assertEquals(result.get(0), "chachucho");
+
+        // ja,ju,jo
+        result = k2r.convert("じゃじゅじょ", KanaToRomaji.SYSTEM_HEPBURN);
+        assertTrue(result.size() == 1);
+        assertEquals(result.get(0), "jajujo");
+        result = k2r.convert("ぢゃぢゅぢょ", KanaToRomaji.SYSTEM_HEPBURN);
+        assertTrue(result.size() == 1);
+        assertEquals(result.get(0), "jajujo");
+    }
+
+    /**
      * かなtoローマ字 ヘボン式 直音変換
      * @throws Exception
      */
