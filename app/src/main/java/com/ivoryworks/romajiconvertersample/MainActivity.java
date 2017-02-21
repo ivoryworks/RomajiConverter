05298cf27f0f81296dd9813e866a66fe293db213
ivoryworks <ivoryworkz@gmail.com>
2017-02-21 23:18:01 +0900

    Libraly join to Sample Application

--- app/src/main/java/com/ivoryworks/romajiconvertersample/MainActivity.java
+++ app/src/main/java/com/ivoryworks/romajiconvertersample/MainActivity.java
@@ -1,7 +1,15 @@
 package com.ivoryworks.romajiconvertersample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.ivoryworks.romajiconverter.KanaToRomaji;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +18,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ((EditText) findViewById(R.id.edit_from)).addTextChangedListener(watchHandler);
+    }
+
+    private TextWatcher watchHandler = new TextWatcher() {
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            KanaToRomaji k2r = new KanaToRomaji();
+            List<String> convertStrings = k2r.convert(charSequence.toString());
+            String strBuf = "";
+            for (String str : convertStrings) {
+                strBuf += str + "\n";
+            }
+            ((TextView) findViewById(R.id.text_result)).setText(strBuf);
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 }
